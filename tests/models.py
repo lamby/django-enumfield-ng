4b85807448c4492329e625e566c23ae315472888
Dan Palmer <dan@danpalmer.me>
2016-07-09 13:28:46 +0100

    Add tests for migrations (currently failing)

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django_enumfield import EnumField
 
 from django.db import models
@@ -8,3 +10,11 @@ from .enums import TestModelEnum
 class TestModel(models.Model):
     test_field = EnumField(TestModelEnum, default=TestModelEnum.A)
     test_field_no_default = EnumField(TestModelEnum)
+
+
+def random_default():
+    return random.choice(TestModelEnum)
+
+
+class TestModelRandomDefault(models.Model):
+    test_field = EnumField(TestModelEnum, default=random_default)
